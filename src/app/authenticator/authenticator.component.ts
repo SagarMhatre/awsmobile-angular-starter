8d0344ac3aedf8701fe236660e0d39290bd1c198
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-04-28 21:47:25 +0530

    Implemented Login Form <-> Dashboard flow

--- src/app/authenticator/authenticator.component.ts
+++ src/app/authenticator/authenticator.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-authenticator',
   template: `
     <amplify-authenticator></amplify-authenticator> 
-  `,
+   `,
   styleUrls: ['authenticator.component.css']
 })
 export class AuthenticatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _amplifyService: AmplifyService, private _router: Router) {
+    this._amplifyService.authStateChange$
+      .subscribe(authState => {
+        if (authState.state === 'signedIn' || authState.state === 'confirmSignIn') {
+          this._router.navigate(['dashboard']);
+        };
+      });
+  }
 
   ngOnInit() {
   }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard',
+  template: `
+    <span class="amplify-greeting-text">{{ greeting }}</span>
+    <button class="amplify-form-button amplify-greeting-sign-out"
+        *ngIf="signedIn"
+        (click)="onSignOut()"
+      >Sign Out</button>
+  `,
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  signedIn: boolean;
+  greeting: string;
+
+  constructor(private _amplifyService: AmplifyService, private _router: Router) {
+    this.subscribe();
+  }
+
+  subscribe() {
+    this._amplifyService.authStateChange$
+      .subscribe(state => this.setAuthState(state));
+  }
+
+  setAuthState(authState) {
+    this.signedIn = (authState.state === 'signedIn' || authState.state === 'confirmSignIn');
+    this.greeting = this.signedIn
+      ? "Hello " + authState.user.username
+      : "";
+    if (!this.signedIn) {
+      this._router.navigate(['login']);
+    }
+  }
+
+  onSignOut() {
+    this._amplifyService.auth().signOut();
+  }
+
+  ngOnInit() {
+  }
+
+}
