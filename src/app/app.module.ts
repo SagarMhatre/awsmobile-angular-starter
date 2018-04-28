732d8c7ef890edc63053f33b349f08e3b971e926
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-04-28 16:19:41 +0530

    Importing amplify lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { AuthenticatorComponent } from './authenticator/authenticator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AmplifyAngularModule
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
