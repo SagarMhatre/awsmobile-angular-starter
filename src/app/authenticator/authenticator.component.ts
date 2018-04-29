e7e1e9c1d931e5b5faa3a20068c3994d6ad2fbaf
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2018-04-29 13:42:49 +0530

    Overriding default css of child components

--- src/app/authenticator/authenticator.component.ts
+++ src/app/authenticator/authenticator.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AmplifyService } from 'aws-amplify-angular';
 import { Router } from "@angular/router";
+import {  ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-authenticator',
   template: `
     <amplify-authenticator></amplify-authenticator> 
    `,
-  styleUrls: ['authenticator.component.scss']
+  styleUrls: ['./authenticator.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AuthenticatorComponent implements OnInit {
 
